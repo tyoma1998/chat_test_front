053c1dac3a0c53c93b548045b395113fdac80045
Artemiy Levitskiy <alevitskiy@jaxel.com>
2022-11-17 18:11:32 +0300

    fixed cors

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,6 +1,11 @@
 import openSocket from "socket.io-client";
 const DOMAIN = process.env.REACT_APP_DOMAIN;
 
-const socket = openSocket(DOMAIN);
+const socket = openSocket(DOMAIN, {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd",
+  },
+});
 
 export default socket;
